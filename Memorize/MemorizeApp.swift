8c04108db4fce9885e119be000f0e41293e749a1
Yuyan Li <yuyanli1211@gmail.com>
2021-12-01 12:28:20 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Yuyan Li on 1/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
